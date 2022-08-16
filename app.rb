11feed730fc9847bc565fbf2b52b774a6da038bf
Justin <jlmyers1993@hotmail.com>
2022-08-16 10:16:21 -0600

    commit tree b13f04a3b113759c46395e5f9e76fd020175b5a3

---
+++ app.rb
@@ -0,0 +1,14 @@
+# app.rb
+# preferred syntax for programmers
+puts "Hello World!"
+# puts "Hello World!"
+# puts "Hello World!"
+
+print "Pass this test, please."
+
+p [1, 2, 3]
+
+# pp [{ id: 1, hello: "World" }, { id: 2, hello: "Ruby" }, { id: 3, hello: "Moon" }, { id: 4, hello: "Learner" }]
+
+# can also be written as:
+# puts("Hello World!")
